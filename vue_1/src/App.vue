14b16b2993f04b22441d19baf4417da7d9a14e72
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-07-17 10:55:47 +0530

    fetching data from an api

--- vue_1/src/App.vue
+++ vue_1/src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {ref} from "vue";
+import {onMounted, ref} from "vue";
 
 const name = ref("Parth")
 const status = ref("active")
@@ -26,6 +26,16 @@ const addTask = () => {
 const deleteTask = (index) => {
     tasks.value.splice(index, 1)
 }
+
+onMounted(async () => {
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/todos")
+        const data = await response.json()
+        tasks.value = data.map((task) => task.title)
+    } catch (error) {
+        console.log("Error fetching api: ", error)
+    }
+})
 </script>
 
 <template>
