06d6024590e40ee8792aaf81d301e0950cf28c35
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-07-17 10:26:31 +0530

    form operations

--- vue_1/src/App.vue
+++ vue_1/src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
 import {ref} from "vue";
 
-const name = "Parth"
+const name = ref("Parth")
 const status = ref("active")
-const tasks = ["Task one", "Task two", "Task three"]
+const tasks = ref(["Task one", "Task two", "Task three"])
+const newTask = ref("")
 
 const changeStatus = () => {
     if (status.value === 'active') {
@@ -13,6 +14,17 @@ const changeStatus = () => {
     } else {
         status.value = 'active'
     }
+}
+
+const addTask = () => {
+    if (newTask.value.valueOf().trim() !== '') {
+        tasks.value.push(newTask.value)
+        newTask.value = ''
+    }
+}
+
+const deleteTask = (index) => {
+    tasks.value.splice(index, 1)
 }
 </script>
 
@@ -25,10 +37,22 @@ const changeStatus = () => {
     <p v-else>user is inactive</p>
     <br>
 
+    <form @submit.prevent="addTask">
+        <label for="newTask">Add Task: </label>
+        <input type="text" id="newTask" name="newTask" v-model="newTask"/>
+        <button type="submit">submit</button>
+    </form>
+    <br>
+
     <h3>Tasks:</h3>
     <br>
     <ul>
-        <li v-for="task in tasks" :key="task">{{ task }}</li>
+        <li v-for="(task, idx) in tasks" :key="task">
+            <span>
+            {{ task }}
+            </span>
+            <button @click="deleteTask(idx)">x</button>
+        </li>
     </ul>
 
     <br>
