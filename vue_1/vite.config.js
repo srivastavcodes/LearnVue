ed9d5bb991e3c98195e950ea27af5c573cb6c8ca
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-07-17 11:33:28 +0530

    tailwind config

--- vue_1/vite.config.js
+++ vue_1/vite.config.js
@@ -1,14 +1,16 @@
 import {fileURLToPath, URL} from 'node:url'
 
 import {defineConfig} from 'vite'
-import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import vue from "@vitejs/plugin-vue";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
-        vue(),
         vueDevTools(),
+        vue(),
+        tailwindcss(),
     ],
     server: {
         port: 3000
