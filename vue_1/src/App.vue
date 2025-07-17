f8d0fe615f3a454fd8e9d7e57a01ce24d46033eb
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-07-17 19:12:49 +0530

    use of computed

--- vue_1/src/App.vue
+++ vue_1/src/App.vue
@@ -1,9 +1,13 @@
 <script setup>
 import Navbar from "@/components/Navbar.vue";
 import Hero from "@/components/Hero.vue";
+import HomeCard from "@/components/HomeCard.vue";
+import JobListings from "@/components/JobListings.vue";
 </script>
 
 <template>
     <Navbar/>
     <Hero/>
+    <HomeCard/>
+    <JobListings :limit="3" :show-button="true"/>
 </template>
---
+++ vue_1/src/components/JobListings.vue
@@ -0,0 +1,33 @@
+<script setup>
+import jobData from "@/jobs copy.json"
+import SingleJob from "@/components/SingleJob.vue";
+import {ref} from "vue";
+
+defineProps({
+    limit: Number,
+    showButton: {
+        type: Boolean,
+        default: false
+    }
+})
+const jobs = ref(jobData)
+</script>
+
+<template>
+    <section class="bg-blue-50 px-4 py-10">
+        <div class="container-xl lg:container m-auto">
+            <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
+                Browse Jobs
+            </h2>
+            <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+                <SingleJob v-for="job in jobs.slice(0, limit) || jobs.length" :key="jobs.id" :job="job"/>
+            </div>
+        </div>
+    </section>
+
+    <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
+        <a href="/jobs"
+            class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >View All Jobs</a>
+    </section>
+</template>
